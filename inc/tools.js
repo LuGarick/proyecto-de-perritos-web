ce5da5d3b3bdc2a051a6210c6511661ad72e8082
selvin <selvingts20@gmail.com>
2024-12-25 22:25:43 -0600

    modified:   inc/tools.js 	renamed:    css/main.css -> public/css/main.css 	renamed:    img/.log-otr/facebook.ico -> public/img/.log-otr/facebook.ico 	renamed:    img/.log-otr/instagram.ico -> public/img/.log-otr/instagram.ico 	renamed:    img/.log-otr/practica de patitas.png -> public/img/.log-otr/practica de patitas.png 	renamed:    img/.log-otr/telegram.ico -> public/img/.log-otr/telegram.ico 	renamed:    img/.log-otr/twitter.ico -> public/img/.log-otr/twitter.ico 	renamed:    img/.perro/pexels-denniz-futalan-2523934.jpg -> public/img/.perro/pexels-denniz-futalan-2523934.jpg 	renamed:    img/.perro/pexels-helena-lopes-3763312.jpg -> public/img/.perro/pexels-helena-lopes-3763312.jpg 	renamed:    img/.perro/pexels-sam-lion-5732504.jpg -> public/img/.perro/pexels-sam-lion-5732504.jpg 	renamed:    img/.perro/pexels-vova-kras-15244433.jpg -> public/img/.perro/pexels-vova-kras-15244433.jpg 	renamed:    img/.slider/pexels-mia-x-12195429.jpg -> public/img/.slider/pexels-mia-x-12195429.jpg 	renamed:    img/.slider/pexels-mihaela-pastiu-9809436.jpg -> public/img/.slider/pexels-mihaela-pastiu-9809436.jpg 	renamed:    img/.slider/pexels-tina-nord-7324407.jpg -> public/img/.slider/pexels-tina-nord-7324407.jpg 	renamed:    js/form.js -> public/js/form.js 	renamed:    js/interface.js -> public/js/interface.js 	renamed:    js/login.js -> public/js/login.js 	renamed:    adopcion.html -> view/adopcion.html 	renamed:    contactanos.html -> view/contactanos.html 	renamed:    index.html -> view/index.html 	renamed:    sobre_nosotros.html -> view/sobre_nosotros.html

--- inc/tools.js
+++ inc/tools.js
@@ -2,13 +2,28 @@
 const express = require('express');
 const app = express();
 const port = 3030;
+const path = require('path');
 
+app.use(express.static(path.join(__dirname,'../public')));
 
 app.get('/', (req,res) =>{
-    res.send("hola mundo");
-})
+    res.sendFile(path.join(__dirname,'../view/index.html'));
+});
+
+app.get('/contactanos.html', (req,res) =>{
+    res.sendFile(path.join(__dirname,'../view/contactanos.html'));
+});
+
+app.get('/adopcion.html', (req,res) =>{
+    res.sendFile(path.join(__dirname,'../view/adopcion.html'));
+});
+
+app.get('/sobre_nosotros.html', (req,res) =>{
+    res.sendFile(path.join(__dirname,'../view/sobre_nosotros.html'));
+});
+
 
 app.listen(port,() => {
     console.log(`servidor ubicado en http://localhost:${port}`);
-})
+});
 
